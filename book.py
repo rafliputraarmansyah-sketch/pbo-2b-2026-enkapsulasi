c9e4c261bd1aa3ea231e3b61abaed017c5955f38
rafliputraarmansyah-sketch <rafliputraarmansyah@gmail.com>
2026-03-13 11:15:24 +0700

    first commit

--- book.py
+++ book.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Book:
     def __init__(self, id_buku, judul, penulis, tahun):
         self.__id_buku = id_buku
@@ -5,3 +7,12 @@ class Book:
         self.penulis = penulis
         self.tahun = tahun
         self.status = "tersedia"
+        self.__update_terakhir = datetime.datetime.now()
+
+    def update_judul(self, judul):
+        self.judul = judul
+        self.__update_terakhir = datetime.datetime.now()
+
+    def get_update_terakhir(self):
+        return self.__update_terakhir
+
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from book import Book
 
 buku1 = Book(1, "Belajar Python", "Affan", 2025)
+buku2 = Book(2, "Belajar Java", "Budi", 2024)
+
 
 print(buku1.judul)
+print(buku1.get_update_terakhir())
+
+buku1.update_judul("Belajar Python Part I")
+
+print(buku1.judul)
+print(buku1.get_update_terakhir())
